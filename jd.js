469b62584a089deb6b6c4a6395b9d581df6a27ff
VBlizzard <bronyanthony@gmail.com>
2013-10-12 19:11:04 -0400

    Create jd.js

---
+++ jd.js
@@ -0,0 +1,19 @@
+//my first code. thank you jd for inspriing me. :P
+confirm("Ready for the best coder's work ever? JK, yet still, ready?");
+console.log ("You are walking in the park. Mind your own business, when suddly, a wildman jumps onto you!");
+
+var jd = prompt (" His name is jd. He looks cocky. Do you fight him?");
+if (jd==="yes") {
+  console.log ("POW! you fight back, and..............you WIN! He starts to bleed and slowly, he fades away.");
+}else{
+  console.log ("You choose not to fight! You got away! YET, he chases you!");
+  
+}
+var jd1 = prompt ("Now he chases you! You soon remember that he saks! Do you fight him?");
+if (jd1==="yes") {
+  console.log ("POW! you win with one blow!");
+}else{
+  console.log ("OH NO! He catches up to you and then..........Glomps you to death. D:");
+  
+}
+  
