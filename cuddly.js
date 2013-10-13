fe6dc060b6dfe5b510bd81e83c679bb3c8cea29a
Anthony <bronyanthony@gmail.com>
2013-10-13 17:51:24 -0400

    Update cuddly.js
    forgot some things.

--- cuddly.js
+++ cuddly.js
@@ -1,9 +1,16 @@
-confirm("ready?");
-console.log ("You are about to be asked the worst question ever.");
-var cuddly=prompt("Is Cuddly awesome?");
-if (cuddly="no); {
-    console.log ("I knew it. I was right all along!");
-}else{
-console.log (" Why must you lie? Cuddly saks.");
+confirm("Ready?");
+var userAge = prompt("What is your age?");
+if (userAge < 1) {
+    console.log("How are you even answering this?");
+} else {
+    console.log("Okay, now to the next question!");
 }
 
+console.log("You are about to be asked the worst question ever.");
+
+var cuddly = prompt("Is Cuddly awesome?");
+if (cuddly = "yes") {
+    console.log("Why must you lie?");
+} else {
+    console.log("I knew it. Cuddly DOES sak.");
+}
