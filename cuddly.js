a2d8c68c99f10dbe660ad0151cd9726f6dc65636
Anthony <bronyanthony@gmail.com>
2013-10-13 17:22:44 -0400

    Create cuddly.js

---
+++ cuddly.js
@@ -0,0 +1,9 @@
+confirm("ready?");
+console.log ("You are about to be asked the worst question ever.");
+var cuddly=prompt("Is Cuddly awesome?");
+if (cuddly="no); {
+    console.log ("I knew it. I was right all along!");
+}else{
+console.log (" Why must you lie? Cuddly saks.");
+}
+
